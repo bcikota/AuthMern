6d8255e66b3fd7f3ec62142e0d0281ab10e99e5c
BogdanCikota <bcikota@live.com>
2021-03-25 20:23:39 +0100

    commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
     cookie: {
-        sameSite: none,
+        sameSite: 'none',
         secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7 //One Week
     }
